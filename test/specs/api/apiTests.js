bdba0515e35eb07c1a979127b8cf3ec6dfb1a683
11420051 <82885357+11420051@users.noreply.github.com>
2023-12-04 14:42:23 +0700

    Fixing ApiTests Script

--- test/specs/api/apiTests.js
+++ test/specs/api/apiTests.js
@@ -1,12 +1,11 @@
 const axios = require('axios');
 const { expect } = require('chai');
 
-    describe('Todo API', async () => {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    describe('Todo API', () => {
         it('should return a list of todos', async () => {
           try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
             expect(response.status).to.equal(200);
-            response.to.have.status(200);
             expect(response.data).to.be.an('array'); 
             expect(response.headers.get('Content-Type')).to.equals('application/json; charset=utf-8');
           } catch (error) {
